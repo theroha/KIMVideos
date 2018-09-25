64a61154b8f2b695cdb0d1ef0d1bddababd67ce9
theroha <jared.strain@yahoo.com>
2018-09-25 07:55:52 -0500

    Update DMX.py
    Converted DMX module into a class.

--- DMX.py
+++ DMX.py
@@ -9,44 +9,49 @@ Function to load a list of DMX values from ENTTEC DMXUSB PRO
 # Import Statements
 import serial, time, os
 
-# Function : Load light settings into a list
-def loadDMXList(serialInput):
-    '''
-    Finds beginning and end of DMX sequence
-    in serial input and returns channel values as a list.
-    '''
-    DMXOPEN = chr(126)
-    DMXCLOSE = chr(231)
-    searching = True
-    
-    while searching:
-        # Flush Input Buffer
-        serialInput.flushInput()
-        
-        # Find Opening Byte
-        opening = 0
-        while opening != DMXOPEN:
-            opening = serialInput.read()
+class DMX:
+    def __init__(self, port):
+        self.serialInput = serial.Serial(port)
+        self.buffer = []
 
-        # Read System Message: See ENTTEC DMXUSB PRO API
-        systemMessage = serialInput.read()
+    # Function : Load light settings into a list
+    def loadDMXList(self):
+        '''
+        Finds beginning and end of DMX sequence
+        in serial input and returns channel values as a list.
+        '''
+        DMXOPEN = chr(126)
+        DMXCLOSE = chr(231)
+        searching = True
 
-        # Read Message Length
-        dataLen = ord(bytearray(serialInput.read()))
-        print dataLen
-        dataLenB = ord(bytearray(serialInput.read()))
-        print dataLenB
+        while searching:
+            # Flush Input Buffer
+            self.serialInput.flushInput()
 
-        # Read data into buffer
-        if dataLen:
-            Buffer = bytearray(serialInput.read(dataLen))
-            LSB = True
-        else:
-            Buffer = bytearray(serialInput.read(dataLenB))
-            LSB = False
+            # Find Opening Byte
+            opening = 0
+            while opening != DMXOPEN:
+                opening = self.serialInput.read()
 
-        final = serialInput.read()
-        if final == DMXCLOSE:
-            searching = False
-    
-    return Buffer
+            # Read System Message: See ENTTEC DMXUSB PRO API
+            systemMessage = self.serialInput.read()
+
+            # Read Message Length
+            dataLen = ord(bytearray(self.serialInput.read()))
+            print dataLen
+            dataLenB = ord(bytearray(self.serialInput.read()))
+            print dataLenB
+
+            # Read data into buffer
+            if dataLen:
+                Buffer = bytearray(self.serialInput.read(dataLen))
+                LSB = True
+            else:
+                Buffer = bytearray(self.serialInput.read(dataLenB))
+                LSB = False
+
+            final = self.serialInput.read()
+            if final == DMXCLOSE:
+                searching = False
+
+        self.buffer = Buffer
